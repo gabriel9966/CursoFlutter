96d293051a54442a29302735794c2404ef4930ff
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-09-26 17:49:39 -0300

    Stateful

--- primeiro_apk/lib/main.dart
+++ primeiro_apk/lib/main.dart
@@ -8,16 +8,43 @@ main() {
 /*StatelessWidget = componente sem estado*/
 // material app = widget inicial da aplicação , da acesso a varias configurações
 //scaffold vai dar a estrutura básica de design da aplicação
-class PerguntaApp extends StatelessWidget {
-  @override
+//column uma lista de elementos organizados verticalmente, ele recebe um childrem(lista de items filhos) como argumento
+
+class PeraguntaAppState extends State<PerguntaApp> {
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      "Qual é sua cor favorita ?",
+      "Qual é sua musica favorita ?",
+      "Qual é seu esporte favorito ?"
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Primeiro"),
         ),
-        body: Text("Ola flutter"),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(onPressed: responder, child: Text("Resposta1")),
+            ElevatedButton(onPressed: responder, child: Text("Resposta2")),
+            ElevatedButton(onPressed: responder, child: Text("Resposta3"))
+          ],
+        ),
       ),
     );
+  }
+
+  var perguntaSelecionada = 0;
+  void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  PeraguntaAppState createState() {
+    return PeraguntaAppState();
   }
 }
