8a45c9190b8a8e4e25772a0e9784a212cef5363e
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-10-02 16:27:15 -0300

    primeiro_apk

--- primeiro_apk/lib/resultado.dart
+++ primeiro_apk/lib/resultado.dart
@@ -1,12 +1,23 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var rd = Random();
+    int Naeatorio = rd.nextInt(101);
+
     return Center(
-        child: Text(
-      "Parabéns !",
-      style: TextStyle(fontSize: 25),
-    ));
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
+        Naeatorio.toString() + " %",
+        style: TextStyle(fontSize: 25),
+      ),
+      Text(
+        "Parabéns !",
+        style: TextStyle(fontSize: 25),
+      )
+    ]));
   }
 }
